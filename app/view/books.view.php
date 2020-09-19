65903759f274175c7409c17f28127d6596741cf7
Lucas Guerrero <guerrerolucas246@gmail.com>
2020-09-19 18:19:20 -0300

    Agregue include 'templates/navbar.php' a la funcion showHome()

--- app/view/books.view.php
+++ app/view/books.view.php
@@ -3,7 +3,7 @@
 class BooksView{
     function showHome(){
         include 'templates/header.php';
-        echo "<h1>TEST</h1>";
+        include 'templates/navbar.php';
         include 'templates/footer.php';
     }
 }
