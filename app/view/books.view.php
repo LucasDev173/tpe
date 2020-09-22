23624f11ff3a1539fca654ad7e6fa0bbe524f8cb
Matias <matiasshedden@gmail.com>
2020-09-21 23:58:43 -0300

    Agregada instruccion SQL para visualizar categorias

--- app/view/books.view.php
+++ app/view/books.view.php
@@ -2,18 +2,18 @@
 
 class BooksView{
 
-    function showHome($libros){
+    function showHome($libros) {
         include 'templates/header.php';
         include 'templates/navbar.php';
         include 'templates/main.php';
-
+    
         echo "<ul class='list-group mt-5'>";
         foreach($libros as $libro) {
             echo "<li class='list-group-item'>
                     TITULO: $libro->titulo <br> 
                     AUTOR: $libro->autor <br>  
                     PRECIO: $libro->precio <br>  
-                    CATEGORIA: $libro->id_categoria
+                    CATEGORIA: $libro->nombre
                 </li>";
         }
         echo "</ul>";
